5e522b02ed11785eec35aba322729b4c163a2b81
Nagaraju <vpp.nagaraju1234@gmail.com>
2019-04-30 16:43:11 +0530

    Subtitle timeline extraction commit

--- subtitle.py
+++ subtitle.py
@@ -13,19 +13,27 @@ parser = ArgumentParser(description='Extract text from srt file \n\r'+
 						"How to Run?\n" +
 						"python3 " + sys.argv[0] + " -i=file.srt " + "-o=all"
 						)
-
-parser.add_argument("-o", "--output_type",
-                    dest="output_type", help="output type=1|2|3|4|ALL",required=True)
 parser.add_argument("-i", "--input", dest="inputfile",
                     help="provide .srt file name",required=True)
+parser.add_argument("-t", "--timeline",
+                    dest="timeline", help="t=yes",required=False)
+parser.add_argument("-o", "--output_type",
+                    dest="output_type", help="output type=1|2|3|4|ALL",required=False)
+
 #parser.add_argument("-details" "--details",
 #                                       dest="detail", help ="Optional-Value is yes or no, default is no")
 
 args = parser.parse_args()
 
 inputfile = args.inputfile
+t = args.timeline
+if(t is not None):
+	t = t.lower()
 output_type = args.output_type
-output_type = output_type.lower()
+if(output_type is not None):
+	output_type = output_type.lower()
+else:
+	output_type = '1'
 
 #extract sentences
 def extractText(i):
@@ -42,9 +50,10 @@ def extractText(i):
 		out_file3 = open('output_file3'+os.path.basename(srtfilename),"w")
 	if(output_type == "4" or output_type == "all"):
 		out_file4 = open('output_file4'+os.path.basename(srtfilename),"w")
-
+	if(t == "yes"):
+		timeline_file = open('timeline_'+os.path.basename(srtfilename),"w")
 	c = 1
-
+	#print(subs)
 	for sub in subs:
 		if(output_type == "1" or output_type == "all"):
 				out_file1.write("[SUBTITLE-"+str(c)+"]\n" + sub.text+"\n") #with placeholder
@@ -54,9 +63,12 @@ def extractText(i):
 				out_file3.write(sub.text + "\n")	#without new lines  and placeholder
 		if(output_type == "4" or output_type == "all"):
 				out_file4.write(sub.text + " ")	#story mode
-		c = c+1
+		if(t == "yes"):
+				timeline_file.write("[SUBTITLE-"+str(c)+"]\n" + str(sub.start)+ " --> " + str(sub.end)+"\n") #with placeholder
+
+		c = c + 1
 		#print(subs.text)
-	print("Text file has been Created Successfully")
+	print("Subtitle processed Successfully!")
 
 
 extractText(inputfile)
