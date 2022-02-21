2307c1d88aec312f43f92fd95140d0248f52c1b0
nagaraju291990 <vpp.nagaraju1234@gmail.com>
2022-02-21 10:59:40 +0530

    handle duplicates accross file, check if exist in bag, if hypheneated words are same on both sides ignore them

--- hyphenated_words.py
+++ hyphenated_words.py
@@ -10,16 +10,30 @@ parser = ArgumentParser(description='This script will align extract hyphenated w
 						)
 parser.add_argument("-i", "--inputfile", dest="inputfile",
 					help="provide input file in xlsx",required=True)
-#parser.add_argument("-o", "--output", dest="outfile",
-#					help="provide outputfilename",required=False)
+parser.add_argument("-b", "--bagfile", dest="bagfile",
+					help="provide bagfilename",required=True)
 
 args = parser.parse_args()
 inputfile = args.inputfile
+bagfilename = args.bagfile
+
+#open list file using open file mode
+fp4 = open(bagfilename, encoding="utf-8") # Open file on read mode
+tgtbag_words = fp4.read().split("\n") # Create a list containing all lines
+fp4.close() # Close file
 
 df = pd.read_excel(inputfile, header=None, na_filter=False)
 df1 = df
 
+tgt_bag_hash = {}
+for tgt in tgtbag_words:
+	if(tgt == ""):
+		continue
+	tgt_split = tgt.split("\t")
+	tgt_bag_hash[tgt_split[1]] = '1'
+
 outfile = re.sub(r'.xlsx', '-out.xlsx', inputfile)
+file_unique_hash = {}
 hyphen_words = []
 for index,row in df.iterrows():
 	#print(row[1])
@@ -28,8 +42,22 @@ for index,row in df.iterrows():
 	words = re.findall(r'[\u0900-\u09FF]+(?:-[\u0900-\u09FF]+)+', tgt)
 	words = list(set(words))
 	words = [re.sub(r'[\?\u0964\,\.,\[\]\;:\'\"]', '', w) for w in words if w]
-	words = ", ".join(words)
-	hyphen_words.append(words)
+	out_words = []
+	for w in words:
+		ww = w.split("-")
+		if(ww[0] == ww[1]):
+			continue
+		if w in tgt_bag_hash:
+			file_unique_hash[w] = 'e'
+			out_words.append(w + '[e]')
+		elif w in file_unique_hash:
+			file_unique_hash[w] = 'd'
+			out_words.append(w + ' [d]')
+		else:
+			file_unique_hash[w] = '1'
+			out_words.append(w)
+	out_words = ", ".join(out_words)
+	hyphen_words.append(out_words)
 	#for word in words:
 
 data = []
